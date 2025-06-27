1de147eeefa201270622253845e49fafd0b1956f
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2025-06-28 04:14:25 +0530

    Welcome Page completed

--- client/src/components/MainContainer.jsx
+++ client/src/components/MainContainer.jsx
@@ -1,30 +1,33 @@
-import React, {useState} from 'react'
+import React from 'react'
+// import { useState } from 'react';
 import "./myStyles.css";
 import Sidebar from './Sidebar';
-import ChatArea from './ChatArea';
+// import ChatArea from './ChatArea';
+import Welcome from "./Welcome";
 
 function MainContainer() {
-  const [conversations, setConversations] = useState([
-    {
-      name: "Test#1",
-      lastMessage: "Last message #1",
-      timeStamp: "today",
-    },
-    {
-      name: "Test#2",
-      lastMessage: "Last Message #2",
-      timeStamp: "today",
-    },
-    {
-      name: "Test#3",
-      lastMessage: "Last Message #3",
-      timeStamp: "today",
-    },
-  ]);
+  // const [conversations, setConversations] = useState([
+  //   {
+  //     name: "Test#1",
+  //     lastMessage: "Last message #1",
+  //     timeStamp: "today",
+  //   },
+  //   {
+  //     name: "Test#2",
+  //     lastMessage: "Last Message #2",
+  //     timeStamp: "today",
+  //   },
+  //   {
+  //     name: "Test#3",
+  //     lastMessage: "Last Message #3",
+  //     timeStamp: "today",
+  //   },
+  // ]);
   return (
     <div className="main-container">
          <Sidebar />
-         <ChatArea  props={conversations[0]}/>
+         <Welcome />
+         {/* <ChatArea  props={conversations[0]}/> */}
     </div>
   )
 }
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from "../Images/live-chat_512px.png";
 
 function Welcome() {
   return (
-    <div>Hii</div>
+    <div className="welcome-container">
+      <img src={logo} alt="Logo" 
+      className="welcome-logo" />
+      <p>View and text directly to people present in the chat Rooms.</p>
+    </div>
   )
 }
 
