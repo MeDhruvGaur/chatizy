142a6739eaa73bfdae3cc604e590f37cadc316ad
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2025-06-28 08:39:12 +0530

    User Groups UI completed

--- client/src/components/CreateGroups.jsx
+++ client/src/components/CreateGroups.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import DoneOutlineRoundedIcon from "@mui/icons-material/DoneOutlineRounded";
+import { IconButton } from '@mui/material';
+
 
 function CreateGroups() {
   return (
-    <div>CreateGroups</div>
+    <div className="createGroups-container">
+        <input placeholder="Enter Group Name"  className="search-box" />
+        <IconButton>
+            <DoneOutlineRoundedIcon />
+        </IconButton>
+    </div>
   )
 }
 
-export default CreateGroups
+export default CreateGroups;
