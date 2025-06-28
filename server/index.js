3d24a88e5e76786daae7075181a0c74a0c3a29e1
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2025-06-28 17:25:03 +0530

    Basic Server setup is completed using node.js and express.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log(Hello);
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res)=>{
+    res.send("Api is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Server is running...."));
